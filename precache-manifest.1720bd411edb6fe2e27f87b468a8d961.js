ac13cb5b829cec8c9136c5a5836e45e321b147a5
Erik Lundberg <erik.lumpa@hotmail.com>
2020-04-20 19:45:01 +0200

    Updates

---
+++ precache-manifest.1720bd411edb6fe2e27f87b468a8d961.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "718f5dcff0af7ff9751b110233fdae5d",
+    "url": "/Teknikspranget/index.html"
+  },
+  {
+    "revision": "076889075a24e5053967",
+    "url": "/Teknikspranget/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "aea24a3d14e7adb55881",
+    "url": "/Teknikspranget/static/css/main.d8a40c49.chunk.css"
+  },
+  {
+    "revision": "076889075a24e5053967",
+    "url": "/Teknikspranget/static/js/2.2d2b2d36.chunk.js"
+  },
+  {
+    "revision": "633ce508062cdafc4f8ece51f4d45ac9",
+    "url": "/Teknikspranget/static/js/2.2d2b2d36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aea24a3d14e7adb55881",
+    "url": "/Teknikspranget/static/js/main.4e8f4498.chunk.js"
+  },
+  {
+    "revision": "d4189359a9145c61fcdc",
+    "url": "/Teknikspranget/static/js/runtime-main.e5efb689.js"
+  },
+  {
+    "revision": "20b8a4dc8b6587f2c0294597053aca13",
+    "url": "/Teknikspranget/static/media/cosupic.20b8a4dc.jpg"
+  },
+  {
+    "revision": "29bf5d04038a2a8b553bbbde0369e7e9",
+    "url": "/Teknikspranget/static/media/erik.29bf5d04.jpg"
+  },
+  {
+    "revision": "172dd26983b067443b59acd79d62b69a",
+    "url": "/Teknikspranget/static/media/portrait.172dd269.jpg"
+  }
+]);
